8f8c995bec2dd8429363c632d72df1c08bc04bdc
gstvg <28798827+gstvg@users.noreply.github.com>
2019-10-28 17:37:26 -0300

    Create createRealtimeSaga.js

---
+++ src/lib/createRealtimeSaga.js
@@ -0,0 +1,27 @@
+import realtimeSaga from 'ra-realtime';
+
+const observeRequest = dataProvider => (type, resource, params) => {
+   
+    return {
+        subscribe(observer) {
+        
+            const query = dataProvider(type, resource, params);
+            const cancelSnapshots = query.onSnapshot(snaphot => {
+              observer.next(snapshot.docs)// New data received, notify the observer
+            })
+
+            const subscription = {
+                unsubscribe() {
+                    // Clean up after ourselves
+                    cancelSnapshots();
+                    // Notify the saga that we cleaned up everything
+                    observer.complete();
+                }
+            };
+
+            return subscription;
+        },
+    };
+};
+
+export default dataProvider => realtimeSaga(observeRequest(dataProvider));
