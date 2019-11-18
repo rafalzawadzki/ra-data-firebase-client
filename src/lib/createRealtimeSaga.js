4ee461c9a858cc45183e1c648b6eeb2e06768512
Gustavo de Bona Garcia <gbonagarcia@gmail.com>
2019-11-17 22:47:58 -0300

    wip

--- src/lib/createRealtimeSaga.js
+++ src/lib/createRealtimeSaga.js
@@ -1,23 +1,31 @@
 import realtimeSaga from 'ra-realtime';
 import Methods from './methods.js';
 
-const observeRequest = dataProvider => (type, resource, params) => {
+const observeRequest = (dataProvider, onDataUpdated = () => {}) => (type, resource, params) => {
   return {
     subscribe(observer) {
       const snapshotParams = Object.assign({}, params);
 
       snapshotParams[Methods.snapshotFlag] = true;
 
+      let isFirst = true;
+
       const cancelSnapshotsPromise = dataProvider(type, resource, snapshotParams).then(query => {
         return query.onSnapshot(
           snapshot => {
+
+            if (!isFirst) {
+              isFirst = false;
+            }
+
+            onDataUpdated(type, resource);
+
             // New data received, notify the observer
             if (snapshot.docs) {
               const docs = snapshot.docs.slice(params.pagination.perPage * (params.pagination.page - 1));
 
               observer.next({
                 data: docs.map(doc => doc.data()),
-                keys: docs.map(doc => doc.id),
                 total: docs.length
               });
             } else {
