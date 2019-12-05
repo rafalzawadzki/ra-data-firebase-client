6b96cc622bb69ecbfb8d328461e37530faf0ed7e
Ezekiel Saturday <ezekielsaturday@Doro-Macbook.local>
2019-12-05 15:01:08 +0100

    removed comsole statment for requests from params

--- src/lib/RestProvider.js
+++ src/lib/RestProvider.js
@@ -84,7 +84,7 @@ const RestProvider = (firebaseConfig = {}, options = {}) => {
     switch (type) {
       case GET_LIST:
         // console.log('GET_LIST');
-        console.log('from ra-data-firestore-json ', type, resourceName, params);
+        // console.log('from ra-data-firestore-json ', type, resourceName, params);
         result = await getList(params, resourceName, resourcesData[resourceName]);
         return result;
       case GET_MANY:
